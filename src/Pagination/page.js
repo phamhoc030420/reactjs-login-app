a164e604af6d8afec4add186a29e489d9569ef96
phamhoc030420 <ctyhoc@gmail.com>
2022-01-21 08:43:12 +0700

    reactjs

--- src/Pagination/page.js
+++
@@ -1,9 +0,0 @@
-import { useState, useEffect } from 'react'
-function Page() {
-    const [pagination, setPagination] = useState({
-        page: 1,
-        limit: 8,
-        totalRows: 1
-    });
-
-}
